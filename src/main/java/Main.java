85f49d8b3bce0708ea88066aed31c98cf11daabe
Stepan <stsalamtat@gmail.com>
2023-03-17 17:00:31 +0300

    first DOM

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\Java\\tools\\chromeDriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
         WebDriver driver  = new ChromeDriver();
         driver.get("https://google.com");
+        WebElement input = driver.findElement(By.xpath("//*[@id=\"input\"]"));
+        input.click();
+
     }
 }
